04fbea49642943d60b8fb1d010c990428e23a2cc
snysly <snysly@umich.edu>
2015-09-19 13:13:09 -0400

    hwk 2 done

--- list.c
+++ list.c
@@ -11,6 +11,30 @@ list_t* insert_sorted(list_t* head, list_t* new_element) {
 	assert(head != NULL);
 	assert(new_element != NULL);
 
+	//edge case: new_element goes in first
+	if(new_element->index <= head->index)
+	{
+		new_element->next = head;
+		return new_element;
+	}
+	list_t* prev_element = head;
+	list_t* next_element = head;
+	while(next_element != NULL && (next_element->index < new_element->index))
+	{
+		prev_element = next_element;
+		next_element = next_element->next;
+	}
+	//check end condition
+	if(next_element== NULL)//means that every element in the list is less than the new_element
+	{
+		prev_element->next = new_element;
+	}
+	else//this means that next_element's index is greater than new_elements
+	{
+		prev_element->next = new_element;
+		new_element->next = next_element;
+	}
+
 	return head;
 }
 
@@ -18,6 +42,24 @@ list_t* insert_sorted(list_t* head, list_t* new_element) {
 // the resulting list. You do not need to preserve the original list.
 list_t* reverse(list_t* head) {
 	assert(head != NULL);
+
+	list_t* next_element = head->next;
+	head->next = NULL;
+	list_t* temp;
+	//next_element will point to the next unreversed node
+	//ie 
+	//		node <-  node <- node  node -> node -> null
+	//				  		   ^    ^
+	//				  	   	   |    |
+	//						  next  head
+	
+	while(next_element != NULL)
+	{
+		temp = next_element->next;
+		next_element->next = head;
+		head = next_element;
+		next_element = temp;
+	}
 
 	return head;
 }
